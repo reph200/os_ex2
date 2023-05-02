9c54200b441f20b33771c2dd8f65b669e91a5a8f
reph200 <raphael.klotnick.43@gmail.com>
2023-05-02 21:18:11 +0300

    jona tests

---
+++ cmake-build-debug/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/main2.cpp.o"
+  "CMakeFiles/tests.dir/resources/uthreads.cpp.o"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/uthreads.h.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uthreads.h.dir/tests/test6_no_out.cpp.o"
+  "CMakeFiles/uthreads.h.dir/uthreads.cpp.o"
+  "CMakeFiles/uthreads.h.dir/utils.cpp.o"
+  "uthreads.h"
+  "uthreads.h.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uthreads.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
